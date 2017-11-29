5b9fbe3e8597dc3bf84a96352d0f1dcbf49167fa
kobalicek <kobalicek.petr@gmail.com>
2017-11-29 17:52:06 +0100

    Added RX580 table to tests

--- test/vbios_test.js
+++ test/vbios_test.js
@@ -6,15 +6,27 @@ const binlib = require("../lib/binlib.js");
 const iofs = require("../lib/iofs.js");
 const vbios = require("../lib/vbios.js");
 
-function log(msg) { console.log(msg); }
+var ok = true;
+var files = fs.readdirSync(__dirname);
 
-const input = fs.readFileSync(__dirname + "/pp_table_xfx.bin");
-const object = vbios.$readObject({ buffer: input, type: vbios.PowerPlayTable, log: log });
+files.forEach(function(fileName) {
+  if (fileName.endsWith(".bin")) {
+    console.log(`PPTable '${fileName}'`);
+    const input = fs.readFileSync(__dirname + "/" + fileName);
+    const object = vbios.$readObject({ buffer: input, type: vbios.PowerPlayTable, log: function(msg) {
+      console.log("  " + msg);
+    }});
 
-const output = Buffer.from(input);
-vbios.$updateObject({ buffer: output, object: object });
+    const output = Buffer.from(input);
+    vbios.$updateObject({ buffer: output, object: object });
 
-const cmp = Buffer.compare(input, output);
-if (cmp !== 0) {
-  console.log("Failed");
-}
+    const cmp = Buffer.compare(input, output);
+    if (cmp !== 0) {
+      ok = false;
+      console.log(`  Read/Write FAILED!`);
+    }
+  }
+});
+
+if (!ok)
+  console.log("\nFAILED! THE LIBRARY IS UNSTABLE");
